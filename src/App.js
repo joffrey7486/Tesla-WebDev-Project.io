ce9fde6bc512b66e8756cb0a22e86d93eca7f8a1
caugnon <joffreycaugnon@gmail.com>
2022-05-28 11:34:21 +0200

    begin navbar header with css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './styles/App.scss';
+import React, { useState} from 'react';
+import Header from './components/Header';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './assets/styles/App.scss';
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  
   return (
-    <div className="App">
-      <h1>Hello Elon</h1>
-    </div>
+    <Router>
+      <div className="app">
+        <h1>Hello Elon</h1>
+        <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+        {/* header block */}
+      </div>
+    </Router>
   );
 }
 
